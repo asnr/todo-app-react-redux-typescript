7b611d5c12828962b7c719eac1a61b4929e13e8b
asnr <asnr@users.noreply.github.com>
2018-07-12 21:17:47 +1000

    Use redux to store state

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -1,31 +1,44 @@
-import * as React from 'react';
 import * as Enzyme from 'enzyme';
+import * as React from 'react';
 
 import Hello from './Hello';
 
+const placeholderProps = {
+  onDecrement: () => {},
+  onIncrement: () => {},
+};
+
 it('renders the correct text when no enthusiasm level is given', () => {
-  const hello = Enzyme.shallow(<Hello name='Daniel' />);
+  const hello = Enzyme.shallow(<Hello {...placeholderProps} name='Daniel' />);
   expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
 });
 
 it('renders the correct text with an explicit enthusiasm of 1', () => {
-  const hello = Enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={1}/>);
+  const hello = Enzyme.shallow(
+    <Hello {...placeholderProps} name='Daniel' enthusiasmLevel={1} />
+  );
   expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
 });
 
 it('renders the correct text with an explicit enthusiasm level of 5', () => {
-  const hello = Enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={5} />);
+  const hello = Enzyme.shallow(
+    <Hello {...placeholderProps} name='Daniel' enthusiasmLevel={5} />
+  );
   expect(hello.find(".greeting").text()).toEqual('Hello Daniel!!!!!');
 });
 
 it('throws when the enthusiasm level is 0', () => {
   expect(() => {
-    Enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={0} />);
+    Enzyme.shallow(
+      <Hello {...placeholderProps} name='Daniel' enthusiasmLevel={0} />
+    );
   }).toThrow();
 });
 
 it('throws when the enthusiasm level is negative', () => {
   expect(() => {
-    Enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={-1} />);
+    Enzyme.shallow(
+      <Hello {...placeholderProps} name='Daniel' enthusiasmLevel={-1} />
+    );
   }).toThrow();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-/* import App from './App'; */
-import Hello from './components/Hello';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { enthusiasm } from './reducers/index';
+import { IStoreState } from './types/index';
+import { EnthusiasmAction } from './actions/index';
+import Hello from './containers/Hello';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'Typescript',
+});
+
 ReactDOM.render(
-  <Hello name="Typescript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
